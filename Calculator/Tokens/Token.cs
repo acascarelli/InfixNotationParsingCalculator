4e1de4a7af558ca69f1fb69cc10afd1fa581723f
Austin Cascarelli <austincascarelli@gmail.com>
2023-04-10 13:08:15 -0700

    added Token namespace with operand, operator classes

---
+++ Calculator/Tokens/Token.cs
@@ -0,0 +1,34 @@
+﻿namespace Calculator.Tokens
+{
+    public interface IToken { }
+
+    public class OperandToken : IToken
+    {
+        public double Value { get; }
+
+        public OperandToken(double value)
+        {
+            Value = value;
+        }
+    }
+
+    public enum OperatorType
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division,
+        OpeningBracket,
+        ClosingBracket
+    }
+
+    public class OperatorToken : IToken
+    {
+        public OperatorType OperatorType { get; }
+
+        public OperatorToken(OperatorType operatorType)
+        {
+            OperatorType = operatorType;
+        }
+    }
+}
