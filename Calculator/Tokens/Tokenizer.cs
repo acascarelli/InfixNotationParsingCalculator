3616b4bbdb5b4a4d6be825ae9b434d0863166279
Austin Cascarelli <austincascarelli@gmail.com>
2023-04-11 10:35:45 -0700

    Added tokenizer testing

--- Calculator/Tokens/Tokenizer.cs
+++ Calculator/Tokens/Tokenizer.cs
@@ -2,11 +2,91 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Calculator.Tokens
 {
-    internal class Tokenizer
+    public class Tokenizer
     {
+        public Tokenizer()
+        {
+            _valueTokenBuilder = new StringBuilder();
+            _infixNotationTokens = new List<IToken>();
+            _validOperators = new char[] { '(', ')', '+', '-', '*', '/' };
+        }
+
+        public IEnumerable<IToken> Parse(string expression)
+        {
+            Reset();
+            foreach (char next in expression)
+            {
+                FeedCharacter(next);
+            }
+            return GetResult();
+        }
+
+        private void FeedCharacter(char next)
+        {
+            if (IsOperatorCharacter(next))
+            {
+                if (_valueTokenBuilder.Length > 0)
+                {
+                    var token = CreateOperandToken(_valueTokenBuilder.ToString());
+                    _valueTokenBuilder.Clear();
+                    _infixNotationTokens.Add(token);
+                }
+
+                var operatorToken = CreateOperatorToken(next);
+                _infixNotationTokens.Add(operatorToken);
+            }
+            else
+            {
+                _valueTokenBuilder.Append(next);
+            }
+        }
+
+        private bool IsOperatorCharacter(char next) => _validOperators.Contains(next);
+
+        private IToken CreateOperandToken(string raw)
+        {
+            if (double.TryParse(raw, out double result))
+                return new OperandToken(result);
+
+            throw new ArgumentException($"The operand {raw} has an invalid format.");
+        }
+
+
+        private IToken CreateOperatorToken(char next) => next switch
+        {
+            '(' => new OperatorToken(OperatorType.OpeningBracket),
+            ')' => new OperatorToken(OperatorType.ClosingBracket),
+            '+' => new OperatorToken(OperatorType.Addition),
+            '-' => new OperatorToken(OperatorType.Subtraction),
+            '*' => new OperatorToken(OperatorType.Multiplication),
+            '/' => new OperatorToken(OperatorType.Division),
+            _ => throw new ArgumentException($"There's not a suitable operator for the char {next}")
+
+        };
+
+        private void Reset()
+        {
+            _valueTokenBuilder.Clear();
+            _infixNotationTokens.Clear();
+        }
+
+        private IEnumerable<IToken> GetResult()
+        {
+            if(_valueTokenBuilder.Length > 0)
+            {
+                var token = CreateOperandToken(_valueTokenBuilder.ToString());
+                _valueTokenBuilder.Clear();
+                _infixNotationTokens.Add(token);
+            }
+
+            return _infixNotationTokens.ToList();
+        }
+
+        private readonly StringBuilder _valueTokenBuilder;
+        private readonly List<IToken> _infixNotationTokens;
+        private readonly char[] _validOperators;
     }
 }
---
+++ CalculatorTests/TokenTests/TokenizerTests.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using Newtonsoft.Json.Linq;
+using Calculator.Tokens;
+
+namespace CalculatorTests.TokenTests
+{
+    public class TokenizerTests
+    {
+        public TokenizerTests()
+        {
+            _tokenizer = new Tokenizer();
+        }
+
+        [Theory]
+        [InlineData("+", OperatorType.Addition)]
+        [InlineData("-", OperatorType.Subtraction)]
+        [InlineData("*", OperatorType.Multiplication)]
+        [InlineData("/", OperatorType.Division)]
+        [InlineData("(", OperatorType.OpeningBracket)]
+        [InlineData(")", OperatorType.ClosingBracket)]
+
+        public void OperatorTest(string expression, OperatorType expectedType)
+        {
+            var actual = _tokenizer.Parse(expression);
+            var expected = new List<IToken>() { new OperatorToken(expectedType) };
+            Compare(actual, expected);
+        }
+
+        [Theory]
+        [InlineData("2.25", 2.25)]
+        [InlineData("0.1", 0.1)]
+        public void CorrectOperandTest(string expression, double expectedValue)
+        {
+            var actual = _tokenizer.Parse(expression);
+            var expected = new List<IToken>() { new OperandToken(expectedValue) };
+            Compare(actual, expected);
+        }
+
+        [Theory]
+        [InlineData("2.25.23")]
+        public void IncorrectOperandTest(string expression)
+        {
+            Action action = () => _tokenizer.Parse(expression);
+            action.Should().ThrowExactly<ArgumentException>();
+        }
+
+        private static void Compare(IEnumerable<IToken> actual, IEnumerable<IToken> expected)
+        {
+            actual.Should().BeEquivalentTo(expected,
+                opts => opts.RespectingRuntimeTypes().WithStrictOrdering()
+            );
+        }
+
+        private readonly Tokenizer _tokenizer;
+    }
+}
