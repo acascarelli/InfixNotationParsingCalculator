628868cf72063b870974d0d778d965166451b374
Austin Cascarelli <austincascarelli@gmail.com>
2023-04-12 11:27:47 -0700

    Working GUI raw string -> infix tokens -> postfix tokens -> calculation -> display GUI result. Changed XAML output label to Viewbox for text shrinking with longer strings.

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,33 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Calculator.Tokens;
+using Calculator.Parsers;
 
 namespace Calculator
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
 
-        private char[] operators = { '+', '-', '*', '/', '{', '}' };
+        private readonly char[] _operators = { '+', '-', '*', '/', '{', '}' };
+        private readonly Tokenizer _tokenizer = new Tokenizer();
+        private readonly ShuntingYardAlgorithm _algorithm = new ShuntingYardAlgorithm();
+        private readonly PostfixNotationCalculator _calculator = new PostfixNotationCalculator();
 
         public MainWindow()
         {
             InitializeComponent();
 
-            resultLabel.Content = "0";
+            resultLabel.Text = "0";
 
             acButton.Click += AcButton_Click;
             negativeButton.Click += NegativeButton_Click;
@@ -37,8 +32,12 @@ namespace Calculator
 
         private void EqualButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            string raw = resultLabel.Text.ToString();
+            var infixNotationTokens = _tokenizer.Parse(raw);
+            var postfixNotationTokens = _algorithm.Apply(infixNotationTokens);
+            var result = _calculator.Calculate(postfixNotationTokens).Value;
 
+            resultLabel.Text = $"{result}";
         }
 
         private void PercentageButton_Click(object sender, RoutedEventArgs e)
@@ -48,52 +47,51 @@ namespace Calculator
 
         private void NegativeButton_Click(object sender, RoutedEventArgs e)
         {
-            char firstToken = resultLabel.Content.ToString().First();
-
-            if (resultLabel.Content.ToString() == "0")
-                return;
-
-            if (operators.Any(c => firstToken.Equals(c)))
-                return;
-
-            resultLabel.Content = $"-{resultLabel.Content}";
-
+            
         }
 
         private void AcButton_Click(object sender, RoutedEventArgs e)
         {
-            resultLabel.Content = "0";
+            resultLabel.Text = "0";
         }
 
         private void OperationButton_Click(object sender, RoutedEventArgs e)
         {
             string selectedOperation = (sender as Button).Content.ToString();
 
-            char lastToken = resultLabel.Content.ToString().Last();
+            char lastToken = resultLabel.Text.ToString().Last();
 
-            if (operators.Any(c => lastToken.Equals(c)))
+            if (_operators.Any(c => lastToken.Equals(c)))
                 return;
             else
-                resultLabel.Content = $"{resultLabel.Content}{selectedOperation}";
+                resultLabel.Text = $"{resultLabel.Text}{selectedOperation}";
         }
 
         private void NumberButton_Click(object sender, RoutedEventArgs e)
         {
-            int selectedValue = int.Parse((sender as Button).Content.ToString());
+            string selectedValue = (sender as Button).Content.ToString();
 
-            if (resultLabel.Content.ToString() == "0")
-                resultLabel.Content = selectedValue;
+            if (resultLabel.Text.ToString() == "0")
+                resultLabel.Text = selectedValue.ToString();
             else
-                resultLabel.Content = $"{resultLabel.Content}{selectedValue}";
+                resultLabel.Text = $"{resultLabel.Text}{selectedValue}";
         }
 
         private void decimalButton_Click(object sender, RoutedEventArgs e)
         {
-            if (resultLabel.Content.ToString().Contains("."))
+            char lastToken = resultLabel.Text.ToString().Last();
+            if (lastToken.Equals('.'))
+            {
                 return;
-            resultLabel.Content = $"{resultLabel.Content}.";
-        }
+            }            
+            else if (_operators.Any(c => lastToken.Equals(c)))
+            {
+                resultLabel.Text = $"{resultLabel.Text}0.";
+            }
+            else
+                resultLabel.Text = $"{resultLabel.Text}.";
+        }        
 
     }
-    
+
 }
